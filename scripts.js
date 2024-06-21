36aa395f29e85fd634acd73a23b467c367e2c557
Sergey Rakhmalevich <rkhmlvch@outlook.com>
2024-06-21 18:51:42 -0400

    Update contact form

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const fullname = document.getElementById("fullname").value;
+      const message = document.getElementById("message").value;
+
+      const subject = `Contact Form Submission from ${fullname}`;
+      const body = `${message}`;
+
+      const mailtoLink = `mailto:someone@example.com?subject=${encodeURIComponent(
+        subject
+      )}&body=${encodeURIComponent(body)}`;
+
+      console.log("Generated mailto link: ", mailtoLink);
+
+      const mailtoAnchor = document.getElementById("mailtoLink");
+      mailtoAnchor.href = mailtoLink;
+      mailtoAnchor.click();
+    });
+});
